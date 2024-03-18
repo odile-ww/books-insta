bcac3d3920d30955f501f735520b344e23828450
Silvia Velicu <odileww@gmail.com>
2024-03-18 15:22:07 +0200

    Deploy app to Firebasee

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDI3Ir8arnqHmPpumTquH4frjPmKU8WE6s',
+  authDomain: 'quotes-api-angular.firebaseapp.com',
+  databaseURL: 'https://quotes-api-angular.firebaseio.com',
+  projectId: 'quotes-api-angular',
+  storageBucket: 'quotes-api-angular.appspot.com',
+  messagingSenderId: '710953902904',
+  appId: '1:710953902904:web:e6cae1d8be92d48e6b17cd',
+};
+
+export const environment = {
+  production: true,
+  firebaseConfig,
+};
